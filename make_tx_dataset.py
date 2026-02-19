1ebb893204bbc4f2847e209efdc32eb4fc921ac7
AlbaVizuetePerez <alvizu.perez2@gmail.com>
2026-02-19 20:03:28 +0100

    Add files via upload

---
+++ make_tx_dataset.py
@@ -0,0 +1,66 @@
+"""
+Este archivo genera transacciones de forma aleatoria que se guardan en un archivo json y son utilizadas para 
+los experimentos. 
+"""
+
+
+import json
+import random
+import time
+
+OUTPUT = "tx_dataset.json"
+
+# CONFIGURACIÓN 
+
+# Balances iniciales 
+BALANCES = {
+    "Banco_001": 1500,
+    "Banco_002": 900,
+    "Banco_003": 1800,
+    "Banco_004": 1100,
+}
+
+BANKS = list(BALANCES.keys())
+
+N_TX = 4 #cantidad de transacciones
+MIN_AMT = 1 #cantidad minima a transferirse en una transacción
+MAX_AMT = 5 #cantidad máxima a transferirse en una transacción
+SEED = 12345 #semilla del generador de números aleatorios
+
+def main():
+    rng = random.Random(SEED)
+
+    # copia local de balances para no crear tx inválidas
+    local = BALANCES.copy()
+    dataset = []
+    base_ts = int(time.time())
+
+    for i in range(N_TX):
+        # elegir sender con fondos
+        candidates = [b for b in BANKS if local[b] >= MIN_AMT]
+        if not candidates:
+            break
+
+        sender = rng.choice(candidates)
+        receiver = rng.choice([b for b in BANKS if b != sender])
+
+        amt = rng.randint(MIN_AMT, min(MAX_AMT, local[sender]))
+        ts = base_ts + i  # timestamps deterministas
+
+        dataset.append({
+            "sender": sender,
+            "receiver": receiver,
+            "amount": amt,
+            "timestamp": ts
+        })
+
+        local[sender] -= amt
+        local[receiver] += amt
+
+    with open(OUTPUT, "w") as f:
+        json.dump(dataset, f, indent=2)
+
+    print(f"Generado {len(dataset)} tx en {OUTPUT}")
+
+if __name__ == "__main__":
+    main()
